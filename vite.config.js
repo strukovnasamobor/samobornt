b3870900c78aa9edf340c1fe6475c12cd69a4f28
ZDamijan <zlatko.damijanic@gmail.com>
2026-08-19 13:16:45 +0200

    feat: AppCheck added

--- vite.config.js
+++ vite.config.js
@@ -30,8 +30,8 @@ export default defineConfig({
     minify: "terser",
     terserOptions: {
       compress: {
-        drop_console: false, // Set to true in production
-        drop_debugger: false, // Set to true in production
+        drop_console: true, // Set to true in production
+        drop_debugger: true, // Set to true in production
       },
     },
   },
