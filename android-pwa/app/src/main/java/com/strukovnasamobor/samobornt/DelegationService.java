d1ff7fcc753175b3c44c6496c7eb50e6b07f5307
ZDamijan <zlatko.damijanic@gmail.com>
2026-08-23 10:08:33 +0200

    added android-pwa

---
+++ android-pwa/app/src/main/java/com/strukovnasamobor/samobornt/DelegationService.java
@@ -0,0 +1,14 @@
+package com.strukovnasamobor.samobornt;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
